000cdd9b7c333616d62248b18bdbc465aa72ec20
tezon <tezonteam@gmail.com>
2023-05-11 07:41:06 -0700

    added file to db

--- controllers/CreateProduct.js
+++ controllers/CreateProduct.js
@@ -1,77 +1,39 @@
 // const productService = require('../services/ProductService')
-const Products = require('../models/product')
+const Products = require("../models/product");
 const asyncHandler = require("express-async-handler");
-const upload = require('../multer');
-
-// exports.createProduct = async (req, res) => {
-//   try {
-//     const product = await productService.createProduct(req.body);
-//     res.json({ data: product, status: "success" });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// };
+const multer = require('multer');
 
 
-exports.createProduct = async (req, res) => {
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+exports.createProduct = upload.single('prodImage'), async (req, res) => {
   try {
-    // handle file upload using multer middleware
-    upload(req, res, async function (err) {
-      if (err) {
-        return res.status(400).json({ message: err });
-      }
-      // get the data from request body and uploaded file
-      const { prodName, prodPrice } = req.body;
-      const prodImage = req.file;
+    const { prodName, prodPrice } = req.body;
+    const prodImage = req.file.path;
 
-      if (!req.body) {
-        res.status(400).json({ message: "Please add all fields" });
-      }
-      
-    
-    
-      const userExists = await Products.findOne({ prodName });
-    
-      if (userExists) {
-        res.status(400).json({ message: "Name already exists" });
-      }
-      
-    
-      const user = await Products.create({
-        prodImage,
-        prodPrice,
-        prodName,
-      });
-    
-      if (user) {
-        res.status(201).json({
-          _id: user.id,
-          prodImage: user.prodImage,
-          prodPrice: user.prodPrice,
-          prodName: user.prodName,
-          message: "Products added successfully",
-        });
-    
-    
-        
-      } else {
-        res.status(400).json({ message: "Invalid User Data" });
-      }
-      return res.status(201).json({ message: 'Product created successfully' });
+    // if (!req.body) {
+    //   res.status(400).json({ message: "Please add all fields" });
+    // }
+
+    const user = new Products({ prodImage,
+      prodPrice,
+      prodName, 
     });
+
+    await user.save();
+
+    return res.status(201).json({ message: "Product created successfully" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Internal Server Error' });
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
-
-
-// exports.getAllProducts = async (req, res) => {
-//   try {
-//     const products = await productService.getAllProduct();
-//     res.json({ data: products, status: "success" });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// };
--- route/productRoute.js
+++ route/productRoute.js
@@ -1,8 +1,56 @@
 const express = require("express")
 const {getAllProducts,createProduct} = require('../controllers/CreateProduct.js') 
 const router = express.Router()
+const multer = require('multer');
+const Products = require("../models/product");
 
-router.post('/', createProduct )
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('prodImage'), async (req, res) => {
+  try {
+    // handle file upload using multer middleware
+
+    const { prodName, prodPrice } = req.body;
+    const prodImage = req.file.path;
+
+    if (!req.body) {
+      res.status(400).json({ message: "Please add all fields" });
+    }
+    const user = new Products({ prodImage,
+      prodPrice,
+      prodName, 
+    });
+
+    await user.save();
+
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        prodPrice: user.prodPrice,
+        prodImage: user.prodImage,
+        prodName: user.prodName,
+        message: "Products added successfully",
+      });
+    } else {
+      res.status(400).json({ message: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+)
 
 
 // router.get("/products", async (req, res) => {
